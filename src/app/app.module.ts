e7c94474c06af0da10db5c4da9963415ab3e1b01
wilfrid benoit <wilfrid.benoit@yahoo.fr>
2021-07-29 11:20:52 +0200

    Add reactive form to encrypt/decrypt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageFormComponent } from './message-form/message-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MessageFormComponent } from './message-form/message-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/message-form/message-form.component.ts
+++ src/app/message-form/message-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-message-form',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageFormComponent implements OnInit {
 
-  constructor() { }
+  cryptAction;
+  decrypt: boolean = false;
+  encrypt: boolean = false;
+  key: string;
+  message: string;
+  messageForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.messageForm = this.fb.group({
+      cryptAction: this.cryptAction,
+      decrypt: this.decrypt,
+      encrypt: this.encrypt,
+      key: this.key,
+      message: this.message,
+    })
   }
 
+  action() {
+    console.log(this.messageForm.getRawValue());
+  }
+
+  decryptAction() {
+    console.log('decrypter');
+  }
+
+  encryptAction() {
+    console.log('encrypter');
+  }
 }
